43c61901459ec68219ea1192b8c8c38d1b129382
Manuel Ulate <fr3zs09@gmail.com>
2022-11-14 16:07:25 -0600

    using httpClientModule

--- src/app/modules/gifs/services/gifs.service.ts
+++ src/app/modules/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,24 +8,35 @@ export class GifsService {
   
   private _apiKey: string = '4HT5jVebpAgV3XuqxzYxA56q25dTD1bd';
 
-  constructor() { }
-
+  
   private _history: string[] = [];
-
+  
   get history() {
     
     return [...this._history];
+    
   }
+  
+  constructor( private http:HttpClient ) { }
+
 
   public searchGifs = ( query: string ) => {
 
     if( !query.trim().length ) return;
+    
     query = query.trim().toLowerCase();
+    
     if( !this._history.includes( query ) ) {
       this._history.unshift ( query );
+      this._history = this._history.splice( 0, 10 );
     }
 
-    this._history = this._history.splice( 0, 10 );
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${ this._apiKey }&q=${ query }&limit=10`)
+      .subscribe( ( resp:any ) => {
+        console.log(resp.data);
+      }
+    );
+
 
   }
 
